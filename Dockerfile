649bf9ab84f57ca6ad57967c41c8f20cdde1bd51
afuous <ben@bheller.me>
2023-04-01 10:22:41 -0700

    Switch to alpine linux

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
-FROM debian:unstable
+FROM alpine:edge
 
-RUN apt-get update -y
-RUN apt-get upgrade -y
-RUN apt-get install -y deluged deluge-web wireguard-tools openresolv procps iptables
+RUN apk add deluge wireguard-tools openresolv procps iptables curl iproute2
 # openresolv and procps are needed by wg-quick, iptables is needed for killswitch
-RUN apt-get install -y curl iproute2
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ gid=$1
 uid=$2
 username=torrenter
 
-groupadd --gid $gid $username
-useradd --uid $uid --gid $gid $username
+addgroup -g $gid $username
+adduser -u $uid -G $username -D -H $username
 
 configfile="/wg-configs/$(ls /wg-configs | shuf -n 1)"
 cp "$configfile" /wg0.conf # need wg0 interface name for killswitch below
@@ -27,7 +27,10 @@ iptables -I OUTPUT -d 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16 -j ACCEPT
 # deluge autologin
 # https://dukrat.net/124/deluge-webui-1-3-6-autologin-disable-password
 # default password is deluge
-sed -i 's/this\.passwordField\.focus(true, 300)/this.onLogin()/' /usr/lib/python3/dist-packages/deluge/ui/web/js/deluge-all-debug.js
+sed -i 's/this\.passwordField\.focus(true,300)/this.onLogin()/' "$(python -c "
+import sysconfig
+print(sysconfig.get_paths()['purelib'])
+")/deluge/ui/web/js/deluge-all.js"
 
 su $username -c 'deluged --config=/deluge-config -d' &
 delugedpid=$!
